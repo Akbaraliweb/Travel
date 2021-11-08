0b6a8d3d92edc4147d15f0543be798a7d146a479
Akbaraliweb <akbaraliwebdevoloper@gmail.com>
2021-11-08 21:32:18 +0300

    travel

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var money = prompt("Sayohat uchun pulni kiriting:");
+console.log("money", money);
+
+var all_use_money = ((500 + 250) * 9433.34 + 120 * 10354.03);
+
+if (money > all_use_money) {
+    alert("“Oq yo’l, Alisher!”");
+}
+else{
+    alert("“Alisher, ozgina sabr qilish kerak bo’lar ekan.”");
+    }
+   
